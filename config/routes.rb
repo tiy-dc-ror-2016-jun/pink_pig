fab36caf8fdfc4577a5914c4c9dcd8061a4cde5e
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-15 14:18:14 -0400

    updated all of conversations view to take in every needed parameter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :media
   # get "companies/new"
   # get "companies/index"
   # post "companies/create"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811202401) do
+ActiveRecord::Schema.define(version: 20160815180132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20160811202401) do
     t.integer  "person_id"
     t.string   "email"
     t.string   "email_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
